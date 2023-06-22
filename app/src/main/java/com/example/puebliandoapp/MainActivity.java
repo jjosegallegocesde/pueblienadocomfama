39b4052748027d4cbb30aa112272d40fb6455f03
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-06-21 20:00:30 -0500

    se crea timer

--- app/src/main/java/com/example/puebliandoapp/MainActivity.java
+++ app/src/main/java/com/example/puebliandoapp/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.puebliandoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    //ATRIBUTOS=ELEMENTOS DE LA ACTIVIDAD
+    MediaPlayer cancion;
+
+    //METODOS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cancion=MediaPlayer.create(this,R.raw.audio);
+        cancion.start();
+
+        //Temporizamos el splash
+        TimerTask inicioAPP=new TimerTask() {
+            @Override
+            public void run() {
+                //lanzo el HOME
+                Intent lanzamiento=new Intent(MainActivity.this,Home.class);
+                startActivity(lanzamiento);
+
+            }
+        };
+        Timer tiempo=new Timer();
+        tiempo.schedule(inicioAPP,10000);
+
     }
 }
